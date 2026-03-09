71ca503c230b1bb4612501bdd3098cd071042165
Dylan Hart <execxd@icloud.com>
2026-03-09 17:56:24 -0500

    rune 1.2.3
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/rune.rb
+++ Formula/rune.rb
@@ -1,13 +1,13 @@
 class Rune < Formula
   desc "A modern CLI text editor that bridges the gap between nano's simplicity and advanced features"
   homepage "https://github.com/exec/rune"
-  version "1.2.0"
+  version "1.2.3"
   license "MIT"
   
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/exec/rune/releases/download/v1.2.0/rune-macos-aarch64.tar.gz"
-      sha256 "dd5605b1b60f35e7b0f519663f1416f07a0522aa2e8b302aeabc431a7325ca12"
+      url "https://github.com/exec/rune/releases/download/v1.2.3/rune-macos-aarch64.tar.gz"
+      sha256 "87d8eb7012c23edfa90c34adcf428a3505f2f54e04213bcd5d0ce0d72955bce2"
     else
       url "https://github.com/exec/rune/releases/download/v1.2.0/rune-macos-x86_64.tar.gz"
       sha256 "a16e49468a82e8d61d70c44448a1f909c87137db3fbfeda808de29b56ea9595b"
