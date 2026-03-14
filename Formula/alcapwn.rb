ae86981f25fd01bfb04b5b089598854aa1ca36a6
Your Name <you@example.com>
2026-03-13 21:12:09 -0500

    Update alcapwn to v2.0.0

--- Formula/alcapwn.rb
+++ Formula/alcapwn.rb
@@ -3,30 +3,19 @@ class Alcapwn < Formula
   homepage "https://github.com/exec/alcapwn"
   license "MIT"
 
-  # Dynamic version detection
-  livecheck do
-    url :stable
-    regex(/v?(\d+(?:\.\d+)+)/i)
-  end
-
-  version do
-    spec = self.class
-    # Fetch the latest tag from the API
-    json = Utils::JSON.parse(Utils::GitHub.open_api("https://api.github.com/repos/exec/alcapwn/releases/latest"))
-    json["tag_name"].gsub(/^v/, "")
-  end
+  version "2.0.0"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/exec/alcapwn/releases/download/v#{version}/alcapwn-macos-aarch64.tar.gz"
+      url "https://github.com/exec/alcapwn/releases/download/v2.0.0/alcapwn_darwin_arm64"
     else
-      url "https://github.com/exec/alcapwn/releases/download/v#{version}/alcapwn-macos-x86_64.tar.gz"
+      url "https://github.com/exec/alcapwn/releases/download/v2.0.0/alcapwn_darwin_amd64"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/exec/alcapwn/releases/download/v#{version}/alcapwn-linux-arm64.tar.gz"
+      url "https://github.com/exec/alcapwn/releases/download/v2.0.0/alcapwn_linux_arm64"
     else
-      url "https://github.com/exec/alcapwn/releases/download/v#{version}/alcapwn-linux-amd64.tar.gz"
+      url "https://github.com/exec/alcapwn/releases/download/v2.0.0/alcapwn_linux_amd64"
     end
   end
 
